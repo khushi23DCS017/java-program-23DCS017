0964435c3f291d4768518a0c07dccb224f49e6a8
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-07-30 10:06:41 +0530

    Create sup8.java

---
+++ part 2/sup8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sup8
+{
+public static void main(String args[])
+{
+String a="The quick brown fox jumps over the lazy dog.";
+String b=a.replace("fox","cat");
+System.out.println(b);
+
+}
+}
