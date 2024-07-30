d1aa649bab463b7362f76745806a81f77ed39478
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-07-30 09:40:16 +0530

    Create bank.java

---
+++ part 1/bank.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class bank
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner (System.in);
+System.out.println("enter balance");
+int b=s.nextInt();
+System.out.println("balance is" + b); 
+ System.out.println(" 23DCS017 Khushi Dadhaniya");
+}
+}
