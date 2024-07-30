d101ae60afab468d08a64f4d2c1d20431bebfdfe
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-07-30 10:11:17 +0530

    Create pra11.java

---
+++ part 2/pra11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class pra11 {
+	
+	
+
+public static void main(String[] args) {
+	String str = "CHARUSAT UNIVERSITY";
+	
+	int a=str.length();
+	System.out.println(a);
+	
+	String lc=str.toLowerCase();
+	System.out.println(lc);
+	
+	String b=str.replace('H','K');
+	System.out.println(b);
+	System.out.println("23DCS017 khushi dadhaniya");
+}
+}
+	
