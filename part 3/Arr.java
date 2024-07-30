265ae2ec957b71b2f92a2aeabcd01f1cec921210
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-07-30 11:04:24 +0530

    Create Arr.java

---
+++ part 3/Arr.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Arr {
+    public static void main(String a[]) 
+	{
+		int x=Integer.parseInt(a[0]);
+		//int y=Integer.parseInt(a[1]);
+		int c=x*100;
+	System.out.println(c);
+	}
+	}
