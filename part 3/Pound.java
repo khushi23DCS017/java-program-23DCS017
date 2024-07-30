6912e3a1f952fffe41c8869bdd8f9b798580a74e
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-07-30 10:13:42 +0530

    Create Pound.java

---
+++ part 3/Pound.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Pound {
+    public static void main(String a[]) 
+	{
+		int x=Integer.parseInt(a[0]);
+		
+		int c=x*100;
+	System.out.println(c);
+	System.out.println("23DCS017 Khushi Dadhaniya");
+	
+	}
+	}
+	
