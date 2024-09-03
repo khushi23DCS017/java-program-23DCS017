42e18008155e9283d81b6e3e8a768d22b0b2c2a0
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-09-03 09:45:10 +0530

    Create Multi.java

---
+++ part 7/Multi.java
@@ -0,0 +1,13 @@
+class Multi extends Thread{
+	public void run()
+	{
+		System.out.println("thread is running");
+		
+	}
+	public static void main(String args[])
+	{
+		Multi t1=new Multi();
+		t1.start();
+		 System.out.println("khushi dadhaniya 23DCS017"); 
+	}
+}
