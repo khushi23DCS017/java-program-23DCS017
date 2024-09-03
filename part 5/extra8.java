0365930e7c3cfba1c4b7a0a2f7ec64462d678281
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-09-03 09:52:15 +0530

    Create extra8.java

---
+++ part 5/extra8.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class extra8
+{
+	
+public static void main(String args[])
+{
+	System.out.println("hello");
+	try{
+		
+		throw new Exception();
+	}
+	catch(Exception e)
+	{
+		System.out.println(e.getMessage());
+		
+	}
+		
+	
+}
+	
+}
