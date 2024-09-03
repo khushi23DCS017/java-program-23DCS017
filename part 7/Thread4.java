541cb18b3f8b5cde8f67d50ff12c02efe7d0f78f
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-09-03 09:47:38 +0530

    Create Thread4.java

---
+++ part 7/Thread4.java
@@ -0,0 +1,34 @@
+class MyThread extends Thread
+{
+	public void run()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Call no.="+i);
+			try
+			{
+			sleep(1000);
+			}
+			catch(InterruptedException e)
+			{
+			e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Thread4
+{
+	public static void main(String []args)
+	{
+		MyThread t1=new MyThread();
+		t1.start();
+		MyThread t2=new MyThread();
+		t2.start();
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("main thread");
+		}
+		System.out.println("khushi dadhaniya 23DCS017"); 
+	}
+}
