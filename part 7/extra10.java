c7c05109ffa08b051718139da8b9c071cd4e94dd
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-09-03 09:37:08 +0530

    Create extra10.java

---
+++ part 7/extra10.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class extra10
+{
+	
+public static void main(String args[])
+{
+	System.out.println("hello");
+	
+		throw new ArithmeticException("charusat / by zero");
+	
+	
+		System.out.println("khushi");
+	
+}
+	
+}
