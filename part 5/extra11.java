3917d35bc5d7f5155f599cc37b7bac42f66865c7
khushi23DCS017 <23dcs017@charusat.edu.in>
2024-09-03 09:54:17 +0530

    Create extra11.java

---
+++ part 5/extra11.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class extra11
+{
+	
+public static void main(String args[])
+{
+	System.out.println("hello");
+	try{
+		
+		throw new Exception("Khushi");
+	}
+	catch(Exception e)
+	{
+		System.out.println(e.getMessage());
+		
+	}
+		
+	System.out.println("How are you..");
+}
+	
+}
